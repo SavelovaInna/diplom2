26cc2bbf1aa98540fb84000bd75f8edc1bd755ca
SavelovaInna <Innasik13@mail.ru>
2018-04-28 15:32:47 +0000

    class fuzzy variables

--- fuzzy_logic_custom.py
+++ fuzzy_logic_custom.py
@@ -4,83 +4,88 @@ import codecs
 from tokenizer import MyTokenizer
 from frequencesCounter import FrequencesCounter
 
+
+class FuzzyVariable:
+    def __init__(self, name, max, count_term):
+        self.name = name
+        self.max = max
+        self.count_term = count_term
+        self.terms = []
+        self.f_arr = np.arange(0, max, 1)
+        self.create_terms()
+
+    def __eq__(self, other):
+        return self.get_word_level() == other
+
+    def create_terms(self):
+        step = (self.max - 1) // (self.count_term - 1)
+        triangle = [0, 0, step]
+        for i in range(0, self.count_term):
+            self.terms.append(fuzz.trimf(self.f_arr, triangle))
+            triangle[0] = triangle[1]
+            triangle[1] = triangle[2]
+            if triangle[2] + step < self.max - 1:
+                triangle[2] = triangle[2] + step
+            else:
+                triangle[2] = self.max - 1
+
+    def create_level(self, input):
+        self.membership = []
+        for term in self.terms:
+            self.membership.append(fuzz.interp_membership(self.f_arr, term, input))
+
+    def get_word_level(self):
+        levels_lower = ['very low', 'low', 'medium low', 'medium', 'medium high', 'high', 'very high']
+        levels_high = ['VL', 'L', 'ML', 'M', 'MH', 'H', 'VH']
+        max_level = self.membership.index(max(self.membership)) + 1
+        if self.count_term == 7:
+            return levels_high[max_level - 1]
+        elif self.count_term == 5:
+            return levels_high[max_level]
+        elif self.count_term == 3:
+            return levels_high[max_level * 2 - 1]
+        elif self.count_term == 2:
+            if max_level == 2:
+                return levels_high[max_level + 3]
+            return levels_high[max_level]
+
+
 fuzzy_vars = dict()
-
-fuzzy_vars['d_char'] = dict()
-fuzzy_vars['d_char']['max'] = 11
-fuzzy_vars['d_char']['count_term'] = 3
-
-fuzzy_vars['d_token'] = dict()
-fuzzy_vars['d_token']['max'] = 13
-fuzzy_vars['d_token']['count_term'] = 3
-
-fuzzy_vars['punck'] = dict()
-fuzzy_vars['punck']['max'] = 38
-fuzzy_vars['punck']['count_term'] = 3
-
-fuzzy_vars['s_token'] = dict()
-fuzzy_vars['s_token']['max'] = 8
-fuzzy_vars['s_token']['count_term'] = 3
-
-fuzzy_vars['space'] = dict()
-fuzzy_vars['space']['max'] = 8
-fuzzy_vars['space']['count_term'] = 3
-
-fuzzy_vars['length'] = dict()
-fuzzy_vars['length']['max'] = 196
-fuzzy_vars['length']['count_term'] = 3
-
-fuzzy_vars['alw_true'] = dict()
-fuzzy_vars['alw_true']['max'] = 2
-fuzzy_vars['alw_true']['count_term'] = 2
-
-for var in fuzzy_vars:
-    fuzzy_vars[var]["f_arr"] = np.arange(0, fuzzy_vars[var]["max"], 1)
-
-x_injection = np.arange(0, 100, 1)
-
-def create_terms(fuzzy_var, max_value, count):
-    step = max_value//(count-1)
-    triangle = [0, 0, step]
-    terms = []
-    for i in range(0, count):
-        terms.append(fuzz.trimf(fuzzy_var, triangle))
-        triangle[0] = triangle[1]
-        triangle[1] = triangle[2]
-        if triangle[2] + step < max_value:
-            triangle[2] = triangle[2] + step
-        else:
-            triangle[2] = max_value
-    return terms
-
-for var in fuzzy_vars:
-    fuzzy_vars[var]["terms"] = create_terms(fuzzy_vars[var]["f_arr"], fuzzy_vars[var]["max"] - 1,
-                                            fuzzy_vars[var]["count_term"])
+fuzzy_vars['d_char'] = FuzzyVariable('d_char', 11, 3)
+fuzzy_vars['d_token'] = FuzzyVariable('d_token', 13, 3)
+fuzzy_vars['punck'] = FuzzyVariable('punck', 38, 3)
+fuzzy_vars['s_token'] = FuzzyVariable('s_token', 8, 3)
+fuzzy_vars['space'] = FuzzyVariable('space', 26, 3)
+fuzzy_vars['length'] = FuzzyVariable('length', 196, 3)
+fuzzy_vars['alw_true'] = FuzzyVariable('alw_true', 2, 2)
 
 
-def create_level(fuzzy_var, terms, input):
-    memberships = []
-    for term in terms:
-        memberships.append(fuzz.interp_membership(fuzzy_var, term, input))
-    return memberships
+def create_out():
+    count_h = 0
+    count_l = 0
+    count_m = 0
+    for var in fuzzy_vars:
+        if fuzzy_vars[var] == 'H':
+            count_h = count_h + 1
+        if fuzzy_vars[var] == 'M':
+            count_m = count_m + 1
+        if fuzzy_vars[var] == 'L':
+            count_l = count_l + 1
 
-def get_word_level(membership):
-    levels_lower = ['very low', 'low', 'medium low', 'medium', 'medium high', 'high', 'very high']
-    levels_high = ['VL', 'L', 'ML', 'M', 'MH', 'H', 'VH']
-    max_level = membership.index(max(membership))
-    step = 7 // len(membership)
-    level = 1
-    if len(membership) == 2:
-        for i in range(0, max_level):
-            level = level + step
-
-        return levels_high[level]
-    else:
-        return levels_high[max_level]
-
+    out = []
+    if count_h > 3:
+        out.append('H')
+    if count_l > 3:
+        out.append('L')
+    if fuzzy_vars['alw_true'] == 'H':
+        out.append('H')
+    if fuzzy_vars['d_char'] == 'M' or fuzzy_vars['d_token'] == 'M':
+        out.append('M')
+    return '-'.join(out)
 
 f = codecs.open('sqliAll.txt', "r", "utf_8")
 out = open("out_variable_fuzzy_level.csv","w")
+out.write('sqli' + '\t' + '\t'.join(fuzzy_vars.keys()) + '\t' + 'output' + '\n')
 tokenizer = MyTokenizer()
 freq_counter = FrequencesCounter()
 
@@ -92,34 +97,14 @@ for line in f:
     memberships = []
 
     for var in fuzzy_vars:
-        membership = create_level(fuzzy_vars[var]["f_arr"],fuzzy_vars[var]["terms"], freq[var])
-        memberships.append(get_word_level(membership))
+        fuzzy_vars[var].create_level(freq[var])
+        memberships.append(fuzzy_vars[var].get_word_level())
 
-    res = '\t'.join(memberships) + '\n'
+    res = '\t'.join(memberships) + '\t' + create_out() + '\n'
     if not line.endswith('\t'):
         out.write(line + '\t' + res)
     else:
         out.write(line + res)
-
-
-# active_rule1 = np.fmax(qual_level_lo, serv_level_lo)
-# tip_activation_lo = np.fmin(active_rule1, tip_lo)
-#
-# tip_activation_lo = np.fmin(active_rule1, tip_lo)  # removed entirely to 0
-#
-# # For rule 2 we connect acceptable service to medium tipping
-# tip_activation_md = np.fmin(serv_level_md, tip_md)
-#
-# # For rule 3 we connect high service OR high food with high tipping
-# active_rule3 = np.fmax(qual_level_hi, serv_level_hi)
-# tip_activation_hi = np.fmin(active_rule3, tip_hi)
-# tip0 = np.zeros_like(x_tip)
-#
-# aggregated = np.fmax(tip_activation_lo,
-#                      np.fmax(tip_activation_md, tip_activation_hi))
-#
-# # Calculate defuzzified result
-# tip = fuzz.defuzz(x_tip, aggregated, 'centroid')
 
 
 
